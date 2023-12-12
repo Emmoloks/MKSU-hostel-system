d4bb48238f0e0d628fbe308e6ace1a8b112cde19
Emmoloks <emmokaloki77@gmail.com>
2023-12-13 02:28:28 +0300

    now building hostels page

--- contact.php
+++ contact.php
@@ -22,25 +22,65 @@
 <div class="container">
   <div class="row">
     <div class="col-lg-6 col-md-6 mb-5 px-4">
+
       <div class="bg-white rounded shadow p-4">
       <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.3942900473594!2d37.260194073960214!3d-1.5308533984548096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f8724f42e161f%3A0xaf5f3242fb4d6ecb!2sMachakos%20University!5e0!3m2!1ssw!2ske!4v1702318882028!5m2!1ssw!2ske" loading="lazy"></iframe>
+
        <h5>Address</h5>
        <a href="https://maps.app.goo.gl/WNeyEK6drbhsrtaAA" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
         <i class="bi bi-geo-alt-fill"></i> XYZ, Machakos University
        </a>
 
+       <h5 class="mt-4">Call us</h5>
+        <a href="tel: +254740868876" class="d-inline-block mb-2 text-decoration-nonetext-dark">
+        <i class="bi bi-telephone-fill"></i> +254741396861
+        </a>
+        <br>
+        <a href="tel: +254740868876" class="d-inline-block text-decoration-nonetext-dark">
+        <i class="bi bi-telephone-fill"></i> +254741396861
+        </a>
+
+        <h5 class="mt-4">Email</h5>
+        <a href="mailto: ask.hearkento@gmail.com" class="d-inline-block text-decoration-nonetext-dark">
+        <i class="bi bi-envelope-at-fill"></i> ask.hearkento@gmail.com
+        </a>
+
+        <h5 class="mt-4">Follow us</h5>
+        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+         <i class="bi bi-twitter-x me-1"></i> 
+        </a>
+        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+        <i class="bi bi-facebook me-1"></i>
+        </a>
+        <a href="#" class="d-inline-block text-dark fs-5">
+        <i class="bi bi-youtube me-1"></i> 
+        </a>
       </div>
     </div>
     <div class="col-lg-6 col-md-6 mb-5 px-4">
-      <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-        <div class="d-flex align--items-center mb-2">
-          <img src="images/wifi-image.jpg" width="100px">
-        </div> 
-        <p>lalallalalladadadadada
-          akkakakakakakakakkkkkkk
-        </p>
+      <div class="bg-white rounded shadow p-4">
+        <form>
+          <h5>Send a message</h5>
+          <div class="mt-3">
+           <label class="form-label" style="font-weight: 500">Name</label>
+           <input type="text" class="form-control shadow-none">
+         </div>
+         <div class="mt-3">
+           <label class="form-label" style="font-weight: 500">Email</label>
+           <input type="email" class="form-control shadow-none">
+         </div>
+         <div class="mt-3">
+           <label class="form-label" style="font-weight: 500">Subject</label>
+           <input type="text" class="form-control shadow-none">
+         </div>
+         <div class="mt-3">
+           <label class="form-label" style="font-weight: 500">Message</label>
+           <textarea class="form-control shadow-none" rows="5" style="resize: none;"></textarea>
+         </div>
+         <button type="submit" class="btn text-white custom-bg mt-3">SEND</button>
+        </form>
       </div>
-
+    </div>
   </div>
 </div>
 
